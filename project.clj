391f114888e033fc8f2f51f3e7f8510306ee2b8d
Zhe Wu <wu@madk.org>
2013-05-07 15:31:09 +0800

    modernize the project & bump version number

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject zpracovani "0.0.1"
+(defproject zpracovani "0.1.0"
   :description "zpracovani: parse.com API client in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.2"]
-                 [clj-http "0.2.2"]]
-  :dev-dependencies [[marginalia "0.7.0-SNAPSHOT"]
-                     [re-rand "0.1.0"]
-                     [lein-clojars "0.6.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.2"]
+                 [clj-http "0.7.2"]]
+  :profiles {:dev {:dependencies [[marginalia "0.7.1"]
+                                  [re-rand "0.1.0"]
+                                  [lein-clojars "0.9.1"]]}})
